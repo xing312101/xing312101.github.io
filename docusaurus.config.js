9762ceca8fe2f83280d71429c87905a2ace5394f
李俞興 <xing312101@gmail.com>
2023-05-29 02:03:28 +0800

    github page setting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,9 +1,11 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
+require('dotenv').config();
+
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const projectRepoUrl = 'https://github.com/xing312101/xing-life'
+const projectRepoUrl = 'https://github.com/xing312101/xing312101.github.io'
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -15,15 +17,17 @@ const config = {
   url: 'https://xing312101.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/xing-life/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'xing312101', // Usually your GitHub org/user name.
-  projectName: 'xing-life', // Usually your repo name.
+  projectName: 'xing312101.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+
+  trailingSlash: true,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
