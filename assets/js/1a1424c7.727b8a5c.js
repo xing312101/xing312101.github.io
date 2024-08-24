c6765ce92512ae6f1d46cee95d7e2a0285ce0482
李俞興 <xing@MacBook-Pro.local>
2024-08-23 22:44:20 -0700

    Deploy website - based on 74383c669e4f899f3805f15f3a841d5e64828013

--- assets/js/1a1424c7.727b8a5c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkxing_life_3=self.webpackChunkxing_life_3||[]).push([[5434],{6119:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/","page":1,"postsPerPage":5,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/814f3328.fce861db.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkxing_life_3=self.webpackChunkxing_life_3||[]).push([[7472],{5513:e=>{e.exports=JSON.parse('{"title":"My Lifes","items":[{"title":"2024-04 Cherry Blossom in Vancouver","permalink":"/vancouver-cherry-blossom-2024","unlisted":false},{"title":"2024-03 Go to Canada","permalink":"/goto-canada-2024","unlisted":false}]}')}}]);
